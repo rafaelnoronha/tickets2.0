bc056347a197f5fa2864c08b3270092e78cc0391
˜Rafael <rafaelsnoronha@hotmail.com>
2023-03-12 14:30:06 -0300

    s

--- apps/perfil/filters.py
+++ apps/perfil/filters.py
@@ -2,6 +2,7 @@ import django_filters
 
 from .models import ClassificacaoPerfil, PerfilUsuario
 from apps.core.filters import lookup_types_string, lookup_types_base, lookup_types_number
+from apps.usuario.filters import UsuarioFilterSet
 
 
 class ClassificacaoPerfilFilterSet(django_filters.FilterSet):
@@ -27,4 +28,16 @@ class PerfilUsuarioFilterSet(django_filters.FilterSet):
             'ps_media_avaliacoes': lookup_types_number,
             'ps_observacoes': lookup_types_string,
         }
+        fields.update([(f'ps_usuario__{chave}', valor) for chave, valor in UsuarioFilterSet.Meta.fields.items()])
+        fields.update([(f'ps_classificacao__{chave}', valor) for chave, valor in ClassificacaoPerfilFilterSet.Meta.fields.items()])
+        fields.update(lookup_types_base)
+
+
+class PerfilUsuarioEmpresaFilterSet(django_filters.FilterSet):
+    class Meta:
+        model = PerfilUsuario
+        fields = {
+            'pm_perfil': ['exact']
+        }
+        fields.update([(f'pm_perfil__{chave}', valor) for chave, valor in PerfilUsuarioFilterSet.Meta.fields.items()])
         fields.update(lookup_types_base)
